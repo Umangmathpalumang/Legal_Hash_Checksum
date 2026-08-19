54fc75c69735aa237568e9b06ca1e2ef144332ae
Umangmathpalumang <umangmathpal@gmail.com>
2026-08-19 21:05:22 +0000

    Add Google Tag Manager and Analytics to CSP script-src

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ async def security_headers(request: Request, call_next):
     # CSP: allows Web Crypto (self), Google Fonts, no inline scripts except hashes
     response.headers["Content-Security-Policy"] = (
         "default-src 'self'; "
-        "script-src 'self' 'unsafe-inline'; "
+        "script-src 'self' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com; "
         "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; "
         "font-src https://fonts.gstatic.com; "
         "img-src 'self' data:; "
